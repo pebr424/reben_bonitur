d4d94cda24ef47e710285b9c40126acc2c92171d
pebr424 <peer.breuer@gmx.de>
2025-12-15 05:29:33 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'reben-bonitur-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
